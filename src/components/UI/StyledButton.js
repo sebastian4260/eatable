750d7595614c762f7eeed4d8efc93dc5a34ec53a
sebastian4260 <br7cervantes@gmail.com>
2021-03-19 11:19:35 -0500

    Add more styles to StyledButton.js

---
+++ src/components/UI/StyledButton.js
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export default styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 16px;
+  background: #fa4a0c;
+  border: none;
+  border-radius: 30px;
+  color: #fff;
+  width: 310px;
+  height: 70px;
+  margin: 0 auto;
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 23px;
+  text-align: center;
+  color: #ffffff;
+`;
