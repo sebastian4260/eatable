55b40c53fafcc2b8253bab1ec0b5a37ca8e759a0
sebastian4260 <br7cervantes@gmail.com>
2021-03-19 11:20:44 -0500

    Create StyledLink Component

---
+++ src/components/UI/StyledLink.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+
+export default styled(Link)`
+  font-family: Source Sans Pro;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 23px;
+  text-align: center;
+  color: #fa4a0c;
+  text-decoration: none;
+`;
